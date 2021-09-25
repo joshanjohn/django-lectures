a7f8dc9c79973ca6dfed3feabb088a5a5e9f720f
Joshanjohn <joshanjohn2003@gmail.com>
2021-09-25 22:27:15 +0530

    start working with add funtionality

--- task/views.py
+++ task/views.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,10 +6,22 @@ from django.shortcuts import render
 
 tasks = ["cook pastha", "clean home", "wask clothes"]
 
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
+
 def task(request):
     return render(request, "task/main.html", {
         "tasks": tasks
     })
 
 def add(request):
-    return render(request, "task/add.html")
+    if request.method == 'POST':
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+
+    return render(request, "task/add.html",{
+        "form": NewTaskForm()
+    })
