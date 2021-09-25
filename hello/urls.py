97b5185f47af053b2a2322ada222089447369c25
Joshanjohn <joshanjohn2003@gmail.com>
2021-09-25 12:27:12 +0530

    hello app bug fix (isitnewyear)

--- hello/urls.py
+++ hello/urls.py
@@ -3,5 +3,5 @@ from .views import index, get_name
 
 urlpatterns = [
     path('', index, name="index"),
-    path('<str:name>/', get_name, name="getName")
+    path('name/<str:name>/', get_name, name="getName")
 ]
--- lecture3/urls.py
+++ lecture3/urls.py
@@ -13,13 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django import urls
 from django.contrib import admin
-from django.urls import path
-from django.urls.conf import include, include
+from django.urls import path, include
 
 urlpatterns = [
-    path('', include("hello.urls")),
+    path("", include("hello.urls")),
     path('admin/', admin.site.urls),
-    path('isitnewyear', include("newyear.urls")),
+    path('isitnewyear/', include("newyear.urls")),
 ]
