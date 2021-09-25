2eee2c153f9f11d8a1406413ce281e00f866abfb
Joshanjohn <joshanjohn2003@gmail.com>
2021-09-25 21:23:32 +0530

    namespace error resolved (when name is same within app url)

--- task/urls.py
+++ task/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "tasks"
 urlpatterns = [
 
-    path("", views.task, name="task"),
+    path("", views.task, name="index"),
     path("add/", views.add, name="add"),
 ]
